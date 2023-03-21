6edacb46e040d131d7277659057d3134a9495688
pawan8979 <p.negi8979@gmail.com>
2023-03-22 00:03:19 +0530

    stringSlice

---
+++ 8stringSlicing.py
@@ -0,0 +1,15 @@
+name= "Pawan,Negi"
+print(name[0:6]) # 0<6
+
+print(len(name)) #length of string
+
+print(name[:5])
+
+print(name[0 : -3]) #len(name)-3
+
+print(name[-3:-1]) # 7:9
+
+#Quiz
+
+nm= "Pawan"
+print(nm[-4 : -2]) # [1:3]
