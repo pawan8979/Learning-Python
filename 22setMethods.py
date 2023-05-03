8c017293e928866c085db2df2a372ceef26c2f55
pawan8979 <p.negi8979@gmail.com>
2023-05-04 01:56:51 +0530

    setMethods

---
+++ 22setMethods.py
@@ -0,0 +1,37 @@
+# I. union() and update():
+# The union() and update() methods prints all items that are present in the two sets. The union() method returns a new set whereas update() method adds item into the existing set from another set.
+
+s1= {1,2,6,5}
+s2= {3,7,6}
+print(s1.union(s2))
+s1.update(s2)
+print(s1)
+
+# II. intersection and intersection_update():
+# The intersection() and intersection_update() methods prints only items that are similar to both the sets. The intersection() method returns a new set whereas intersection_update() method updates into the existing set from another set.
+
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+cities3 = cities.intersection(cities2)
+print(cities3)
+cities.intersection_update(cities2)
+print(cities)
+
+# III. symmetric_difference and symmetric_difference_update():
+# The symmetric_difference() and symmetric_difference_update() methods prints only items that are not similar to both the sets. The symmetric_difference() method returns a new set whereas symmetric_difference_update() method updates into the existing set from another set.
+
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+cities3 = cities.symmetric_difference(cities2)
+print(cities3)
+cities.symmetric_difference_update(cities2)
+print(cities)
+
+# IV. difference() and difference_update():
+# The difference() and difference_update() methods prints only items that are only present in the original set and not in both the sets. The difference() method returns a new set whereas difference_update() method updates into the existing set from another set.
+
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2 = {"Seoul", "Kabul", "Delhi"}
+cities3 = cities.difference(cities2)
+print(cities3)
+print(cities.difference(cities2))
