a270e9f755b72862cabc661b3be2eaf74b7d24d5
pawan8979 <p.negi8979@gmail.com>
2023-05-04 12:36:10 +0530

    finallyClause

---
+++ 26finallyClause.py
@@ -0,0 +1,67 @@
+"""
+----------------------------------------------------
+The finally code block is also a part of exception handling. When we handle exception using the try and except block, we can include a finally block at the end. The finally block is always executed, so it is generally used for doing the concluding tasks like closing file resources or closing database connection or may be ending the program execution with a delightful message.
+
+Syntax:
+try:
+   #statements which could generate 
+   #exception
+except:
+   #solution of generated exception
+finally:
+    #block of code which is going to 
+    #execute in any situation
+
+The finally block is executed irrespective of the outcome of try……except…..else blocks
+One of the important use cases of finally block is in a function which returns a value.
+----------------------------------------------------
+"""
+
+try:
+    num = int(input("Enter an integer: "))
+except ValueError:
+    print("Number entered is not an integer.")
+else:
+    print("Integer Accepted.")
+finally:
+    print("This block is always executed.")
+
+
+#Why to use finally instead of a simple print statement?
+
+# Not using finally:
+def func1():
+    try:
+        l = [1, 5, 6, 7]
+        i = int(input("Enter the index: "))
+        print(l[i])
+        return 1
+    except:
+        print("Some error occurred")
+        return 0
+
+    print("I am always executed") #this will not execute because of return statement
+
+x = func1()
+print(x)
+
+# Using finally:
+
+x = func1()
+print(x)
+
+def func1():
+  try:
+    l = [1, 5, 6, 7]
+    i = int(input("Enter the index: "))
+    print(l[i])
+    return 1
+  except:
+    print("Some error occurred")
+    return 0
+
+  finally:
+    print("I am always executed") #will be executed everytime
+
+x = func1()
+print(x)
